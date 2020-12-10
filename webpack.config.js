a8ae12c931a178f53fa97ae4e67b8c27218beca4
Neil <neilrowland56@yahoo.com>
2020-12-10 09:03:52 +0000

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+
+const path = require('path')
+
+module.exports = {
+  entry: {
+    app: ['@babel/polyfill', './src/app.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  
+}
